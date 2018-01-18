de63f84bd4d5a3c4fb903b7dfd54afea37136144
shaheermalik <31680920+shaheermalik@users.noreply.github.com>
2018-01-18 08:25:37 -0500

    Initial commit

---
+++ src/code.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class code {
+
+
+         public static void main(String[] args) {
+
+
+             String Username;
+             String Password;
+
+
+             Password = "password";
+             Username = "hm2123";
+
+
+             Scanner input1 = new Scanner(System.in);
+             System.out.println("Enter Username : ");
+             String username = input1.next();
+
+
+             Scanner input2 = new Scanner(System.in);
+             System.out.println("Enter Password : ");
+             String password = input2.next();
+
+
+             if (username.equals(Username) && password.equals(Password)) {
+
+
+                     System.out.println("Access Granted! Welcome!");
+                 }
+
+
+             else if (username.equals(Username)) {
+                     System.out.println("Invalid Password!");
+                 } else if (password.equals(Password)) {
+                     System.out.println("Invalid Username!");
+                 } else {
+                     System.out.println("Invalid Username & Password!");
+                 }
+
+
+
+         }
+         }
---
+++ src/even.java
@@ -0,0 +1,20 @@
+public class even {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i <= 100) {
+            System.out.print(" " + i);
+            i += 2;
+
+            if (i % 10 == 0)
+
+
+                System.out.println();
+
+
+        }
+
+    }
+
+}
